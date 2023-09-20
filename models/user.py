253b2b4c8308b703d833860a9d27f918ef8fe54b
THOSSYN <mtafortunefinder@gmail.com>
2023-09-20 00:29:46 -0700

    Update models/user.py and models/city.py for Task 8

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.review import Review
 from sqlalchemy import Column, String, Float
 from sqlalchemy.orm import relationship
 
@@ -8,9 +9,12 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False, default="")
+    last_name = Column(String(128), nullable=False, default="")
     places = relationship("Place", backref="user",
                           cascade="all, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete-orphan")
