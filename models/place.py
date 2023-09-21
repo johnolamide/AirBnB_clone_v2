c22273e6e64ca06c74fcc66086681b0bd400a4fa
THOSSYN <mtafortunefinder@gmail.com>
2023-09-21 03:33:42 -0700

    Corrected models/place.py

--- models/place.py
+++ models/place.py
@@ -16,14 +16,13 @@ class Place(BaseModel, Base):
                      nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False,
-                         default="No description found")
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False, default=0.0)
-    longitude = Column(Float, nullable=False, default=0.0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
 
     storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
