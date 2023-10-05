c808fb21762e691a39a2484e9c1e4004f43b7920
Olamide Alokan <alokanolamide@gmail.com>
2023-10-05 14:13:59 +0100

    [ADD] 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
+""" this script contains the function definition for do_pack
+"""
+from fabric.api import local
+from datetime import datetime
+from collections.abc import Mapping
 
+
+def do_pack():
+    """ creates a .tgz archive from web_static folder
+    """
+    local("mkdir -p versions")
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.tgz".format(timestamp)
+
+    result = local("tar -czvf {} web_static".format(archive_name))
+
+    if result.failed:
+        return None
+    else:
+        return archive_name
