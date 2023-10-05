68e4373fed8ebe5ef768b8c3144c541efc54c61c
Olamide Alokan <alokanolamide@gmail.com>
2023-10-05 15:45:45 +0100

    [ADD] 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" this script contains the function definition of do_deploy
+"""
+from fabric.api import env, put, run, local
+from datetime import datetime
+import os
+
+
+env.hosts = ['54.210.42.244', '54.160.120.100']
+def do_deploy(archive_path):
+    """ Distributes and deploy an archive to webservers
+        Args:
+            archive_path (str): path to the archive
+    """
+    if not os.path.exists(archive_name):
+        return False
+
+    try:
+        archive_basename = os.path.basename(archive_path)
+        archive_filename = os.path.splittext(archive_basename)[0]
+        release_path = '/data/web_static/releases/{}'.format(archive_filename)
+
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}".format(release_path))
+        run("tar -xzf /tmp/{} -C {}".format(archive_basename, release_path))
+
+        run("rm /tmp/{}".format(archive_basename))
+
+        current_path = '/data/web_static/current'
+        run("rm -f {}".format(current_path))
+        run("ln -s {} {}".format(release_path, current_path))
+
+        print("New version deployed")
+        return True
+    except:
+        return False
