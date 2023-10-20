1750d66645d26923a933c7f71d80dc8e3e655dd1
Olamide Alokan <alokanolamide@gmail.com>
2023-10-20 15:24:53 +0100

    [ADD] 5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Script starts a Flask web application
+"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Return "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Return "C <text>"
+    """
+    text = text.replace('_', ' ')
+    return f"C {escape(text)}"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ Return "Python <text>"
+    """
+    text = text.replace('_', ' ')
+    return f"Python {escape(text)}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """ display 'n' if it is an integer
+    """
+    return f"{escape(n)} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ render a template with the number passed
+    """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
