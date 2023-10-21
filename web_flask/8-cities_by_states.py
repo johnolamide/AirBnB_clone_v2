c3e466e64faaa37fb4eb7384faf66f8abefd354f
Olamide Alokan <alokanolamide@gmail.com>
2023-10-21 18:52:36 +0100

    cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script starts a flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    """ prints the list of states in the storage
+    """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html',
+                           states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ teardown the db
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
