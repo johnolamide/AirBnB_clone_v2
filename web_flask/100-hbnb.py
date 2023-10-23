02f47b6d19e86a3badf3ab3a339eb423c5378b08
Olamide Alokan <alokanolamide@gmail.com>
2023-10-23 02:09:27 +0100

    HBNB is alive

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -6,6 +6,7 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 
 
 app = Flask(__name__)
@@ -13,14 +14,17 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     """ prints the list of states in the storage
     """
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html',
-                           states=states, amenities=amenities)
+    places = storage.all(Place).values()
+    return render_template('100-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places)
 
 
 @app.teardown_appcontext
